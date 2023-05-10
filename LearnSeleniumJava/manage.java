16274bdc95849adf2b79349e467c8c5a9bafd4a5
Hüseyin <87951691+Huseyinoney@users.noreply.github.com>
2023-05-10 14:45:50 +0300

    Update manage.java

--- LearnSeleniumJava/manage.java
+++ LearnSeleniumJava/manage.java
@@ -3,26 +3,32 @@ package test.Example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Navigate {
-
-    public static void main(String[] args) {
+public class manage {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","chromeDriverPath");
         WebDriver driver = new ChromeDriver();
 
-        //amazon ana sayfasına git.
-        driver.navigate().to("https://amazon.com");
+        System.out.println("sayfanın konumu"+driver.manage().window().getPosition());
+        System.out.println("sayfanın boyutları"+driver.manage().window().getSize());
 
-        //youtube ana sayfasına git
-        driver.navigate().to("https://youtube.com");
-        //amazon sayfasına geri dön
-        driver.navigate().back();
+        //sayfayı simge durumuna getir
+        driver.manage().window().minimize();
 
-        //yeniden youtube sayfasına git
-        driver.navigate().forward();
-        //sayfayı yenile
-        driver.navigate().refresh();
-        //sayfayı kapat
+        //simge durumunda 3 saniye bekle ve maximize yap
+        Thread.sleep(3000);
+        driver.manage().window().maximize();
+
+        //sayfanın konumunu ve boyutunu maximize olarak yazdır
+        System.out.println("sayfanın konumu"+driver.manage().window().getPosition());
+        System.out.println("sayfanın boyutları"+driver.manage().window().getSize());
+
+        //sayfayı fullscreen yap
+        driver.manage().window().fullscreen();
+        System.out.println("sayfanın konumu"+driver.manage().window().getPosition());
+        System.out.println("sayfanın boyutları"+driver.manage().window().getSize());
+        Thread.sleep(3000);
+
         driver.close();
-
     }
+
 }
